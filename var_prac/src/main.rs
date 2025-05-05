7555d2707f0c695148a44cb8c0f4799c2b386736
Rachit Patel <rachitpatel014@gmail.com>
2025-05-05 15:27:13 +0530

    Added Variables
    Learned and practised variable definitions

---
+++ var_prac/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, Var!");
+
+    let mut a = 10;
+    println!("{}", a);
+
+    a = 20;
+    println!("{}", a);
+
+    let b = "My name is John!";
+    println!("{}", b);
+}
