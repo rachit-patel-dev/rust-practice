d37b28d76eb9aeed168401974cd388d3050346ca
Rachit Patel <rachitpatel014@gmail.com>
2025-05-06 12:10:10 +0530

    Added If Else
    Learned and practised if-else conditions

---
+++ if_else_prac/target/debug/deps/if_else_prac-791a7e01020f2d68.d
@@ -0,0 +1,9 @@
+/Users/Sync/Desktop/rust-learning/if_else_prac/target/debug/deps/libif_else_prac-791a7e01020f2d68.rmeta: src/main.rs Cargo.toml
+
+/Users/Sync/Desktop/rust-learning/if_else_prac/target/debug/deps/if_else_prac-791a7e01020f2d68.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ if_else_prac/target/debug/deps/if_else_prac-fef6de8e2d024fde.d
@@ -0,0 +1,9 @@
+/Users/Sync/Desktop/rust-learning/if_else_prac/target/debug/deps/libif_else_prac-fef6de8e2d024fde.rmeta: src/main.rs Cargo.toml
+
+/Users/Sync/Desktop/rust-learning/if_else_prac/target/debug/deps/if_else_prac-fef6de8e2d024fde.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
