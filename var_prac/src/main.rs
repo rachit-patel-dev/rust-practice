8e1c08b10c5bbd2b84b8bdc360a3acd999abcd92
Rachit Patel <rachitpatel014@gmail.com>
2025-05-12 17:57:41 +0530

    Updated Variables Practise
    Updated variables to have constant and scope values.

--- var_prac/src/main.rs
+++ var_prac/src/main.rs
@@ -9,4 +9,20 @@ fn main() {
 
     let b = "My name is John!";
     println!("{}", b);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECINDS: u32 = 60 * 60 * 3;
+    println!("3 hours = {} seconds", THREE_HOURS_IN_SECINDS);
+
+    let q = 5;
+    let q = q + 1;
+    {
+        let q = q * 2;
+        println!("The inner value of q is {}", q);
+    }
+    println!("The outer value of q is {}", q);
 }
